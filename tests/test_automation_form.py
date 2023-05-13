98c442c8f22b37eb8f34c09e9d119e0226a322b6
Silver <testingbugsrus@gmail.com>
2023-05-13 15:57:25 +0300

    Changes in methods

--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -18,25 +18,25 @@ class RegistrationPage:
     def choose_gender(self, gender):
         browser.element(f'[value={gender}] + label').click()
 
-    def fill_tel_number(self):
-        browser.element('#userNumber').type('1234567890')
+    def fill_tel_number(self, number):
+        browser.element('#userNumber').type(number)
 
-    def fill_birthdate(self):
+    def fill_date_of_birth(self, month, year, day):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').click()
         browser.all('.react-datepicker__month-select option') \
             .should(have.size_greater_than(0)) \
-            .filtered_by(have.exact_text('September')).first.click()
+            .filtered_by(have.exact_text(month)).first.click()
         browser.element('.react-datepicker__year-select').click()
         browser.all('.react-datepicker__year-select option') \
             .should(have.size_greater_than(0)) \
-            .filtered_by(have.exact_text('1985')).first.click()
-        browser.element('.react-datepicker__day--004') \
+            .filtered_by(have.exact_text(year)).first.click()
+        browser.element(f'.react-datepicker__day--00{day}') \
             .should(have.no.css_class('.react-datepicker__day--outside-month')).click()
 
-    def fill_subjects(self):
-        browser.element('#subjectsInput').type('Math').press_enter()
-        browser.element('#subjectsInput').type('Physics').press_enter()
+    def fill_subjects(self, first_subject, second_subject):
+        browser.element('#subjectsInput').type(first_subject).press_enter()
+        browser.element('#subjectsInput').type(second_subject).press_enter()
 
     def choose_hobies(self):
         for index in range(0, 3):
@@ -44,32 +44,33 @@ class RegistrationPage:
                 .should(have.size_greater_than(0)) \
                 .element(index).click()
 
-    def uplod_picture(self):
-        browser.element('#uploadPicture').set_value(os.path.join(os.path.abspath('..\\resources'), 'test_img.jpg'))
+    def uplod_picture(self, file_name):
+        browser.element('#uploadPicture').set_value(os.path.join(os.path.abspath('..\\resources'), file_name))
 
-    def fill_current_address(self):
-        browser.element('#currentAddress').type('Nizhny Novgorod')
+    def fill_current_address(self, address):
+        browser.element('#currentAddress').type(address)
 
-    def choose_locatio(self):
+    def choose_locatio(self, state, city):
         browser.element('#state').click()
-        browser.element('#react-select-3-option-2').should(have.exact_text('Haryana')).click()
+        browser.element('#react-select-3-option-2').should(have.exact_text(state)).click()
         browser.element('#city').click()
-        browser.element('#react-select-4-option-1').should(have.exact_text('Panipat')).click()
+        browser.element('#react-select-4-option-1').should(have.exact_text(city)).click()
         browser.element('#submit').click()
 
-    def should_registered_user_whith(self):
+    def should_registered_user_whith(self, full_name, email, gender, tel_number, date_of_birth, subjects, hobies,
+                                     file_name, current_address, state_and_city):
         browser.element('.modal-title').should(have.exact_text('Thanks for submitting the form'))
         browser.element('.table').all('td').even.should(have.exact_texts(
-            'Aleksandr Pushkin',
-            'Pushkin@proton.ru',
-            'Male',
-            '1234567890',
-            '04 September,1985',
-            'Maths, Physics',
-            'Sports, Reading, Music',
-            'test_img.jpg',
-            'Nizhny Novgorod',
-            'Haryana Panipat'
+            full_name,
+            email,
+            gender,
+            tel_number,
+            date_of_birth,
+            subjects,
+            hobies,
+            file_name,
+            current_address,
+            state_and_city
         ))
 
 
@@ -83,18 +84,29 @@ def test_form():
 
     registration_page.choose_gender('Male')
 
-    registration_page.fill_tel_number()
+    registration_page.fill_tel_number('1234567890')
 
-    registration_page.fill_birthdate()
+    registration_page.fill_date_of_birth('September', '1985', '4')
 
-    registration_page.fill_subjects()
+    registration_page.fill_subjects('Math', 'Physics')
 
     registration_page.choose_hobies()
 
-    registration_page.uplod_picture()
+    registration_page.uplod_picture('test_img.jpg')
 
-    registration_page.fill_current_address()
+    registration_page.fill_current_address('Nizhny Novgorod')
 
-    registration_page.choose_locatio()
+    registration_page.choose_locatio('Haryana', 'Panipat')
 
-    registration_page.should_registered_user_whith()
+    registration_page.should_registered_user_whith(
+        'Aleksandr Pushkin',
+        'Pushkin@proton.ru',
+        'Male',
+        '1234567890',
+        '04 September,1985',
+        'Maths, Physics',
+        'Sports, Reading, Music',
+        'test_img.jpg',
+        'Nizhny Novgorod',
+        'Haryana Panipat'
+    )
