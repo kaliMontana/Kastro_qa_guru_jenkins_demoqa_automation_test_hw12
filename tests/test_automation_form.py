22fef65a84854def2ed51eb59ba16bddd1c971d4
Silver <testingbugsrus@gmail.com>
2023-05-13 12:34:44 +0300

    Add methods for steps

--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -3,55 +3,98 @@ import os
 from selene import browser, have
 
 
-def test_form():
-    browser.open('/automation-practice-form')
-    browser.element('.main-header').should(have.exact_text('Practice Form'))
+class RegistrationPage:
+    def open(self):
+        browser.open('/automation-practice-form')
+        browser.element('.main-header').should(have.exact_text('Practice Form'))
 
-    browser.element('#firstName').type('Aleksandr')
-    browser.element('#lastName').type('Pushkin')
-    browser.element('#userEmail').type('Pushkin@proton.ru')
-    browser.element('[for=gender-radio-1]').click()
-    browser.element('#userNumber').type('1234567890')
+    def fill_name(self):
+        browser.element('#firstName').type('Aleksandr')
+        browser.element('#lastName').type('Pushkin')
 
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.all('.react-datepicker__month-select option') \
-        .should(have.size_greater_than(0)) \
-        .filtered_by(have.exact_text('September')).first.click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.all('.react-datepicker__year-select option') \
-        .should(have.size_greater_than(0)) \
-        .filtered_by(have.exact_text('1985')).first.click()
-    browser.element('.react-datepicker__day--004') \
-        .should(have.no.css_class('.react-datepicker__day--outside-month')).click()
+    def fill_email(self):
+        browser.element('#userEmail').type('Pushkin@proton.ru')
 
-    browser.element('#subjectsInput').type('Math').press_enter()
-    browser.element('#subjectsInput').type('Physics').press_enter()
+    def choose_gender(self):
+        browser.element('[for=gender-radio-1]').click()
 
-    for index in range(0, 3):
-        browser.all('#hobbiesWrapper .custom-control-label') \
+    def fill_tel_number(self):
+        browser.element('#userNumber').type('1234567890')
+
+    def fill_birthdate(self):
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__month-select').click()
+        browser.all('.react-datepicker__month-select option') \
             .should(have.size_greater_than(0)) \
-            .element(index).click()
+            .filtered_by(have.exact_text('September')).first.click()
+        browser.element('.react-datepicker__year-select').click()
+        browser.all('.react-datepicker__year-select option') \
+            .should(have.size_greater_than(0)) \
+            .filtered_by(have.exact_text('1985')).first.click()
+        browser.element('.react-datepicker__day--004') \
+            .should(have.no.css_class('.react-datepicker__day--outside-month')).click()
 
-    browser.element('#uploadPicture').set_value(os.path.join(os.path.abspath('..\\resources'), 'test_img.jpg'))
+    def fill_subjects(self):
+        browser.element('#subjectsInput').type('Math').press_enter()
+        browser.element('#subjectsInput').type('Physics').press_enter()
 
-    browser.element('#currentAddress').type('Nizhny Novgorod')
-    browser.element('#state').click()
-    browser.element('#react-select-3-option-2').should(have.exact_text('Haryana')).click()
-    browser.element('#city').click()
-    browser.element('#react-select-4-option-1').should(have.exact_text('Panipat')).click()
-    browser.element('#submit').click()
+    def choose_hobies(self):
+        for index in range(0, 3):
+            browser.all('#hobbiesWrapper .custom-control-label') \
+                .should(have.size_greater_than(0)) \
+                .element(index).click()
 
-    browser.element('.modal-title').should(have.exact_text('Thanks for submitting the form'))
-    browser.all('tbody tr').should(have.exact_texts(
-        'Student Name Aleksandr Pushkin',
-        'Student Email Pushkin@proton.ru',
-        'Gender Male',
-        'Mobile 1234567890',
-        'Date of Birth 04 September,1985',
-        'Subjects Maths, Physics',
-        'Hobbies Sports, Reading, Music',
-        'Picture test_img.jpg',
-        'Address Nizhny Novgorod',
-        'State and City Haryana Panipat'
-    ))
+    def uplod_picture(self):
+        browser.element('#uploadPicture').set_value(os.path.join(os.path.abspath('..\\resources'), 'test_img.jpg'))
+
+    def fill_current_address(self):
+        browser.element('#currentAddress').type('Nizhny Novgorod')
+
+    def choose_locatio(self):
+        browser.element('#state').click()
+        browser.element('#react-select-3-option-2').should(have.exact_text('Haryana')).click()
+        browser.element('#city').click()
+        browser.element('#react-select-4-option-1').should(have.exact_text('Panipat')).click()
+        browser.element('#submit').click()
+
+    def should_registered_user_whith(self):
+        browser.element('.modal-title').should(have.exact_text('Thanks for submitting the form'))
+        browser.all('tbody tr').should(have.exact_texts(
+            'Student Name Aleksandr Pushkin',
+            'Student Email Pushkin@proton.ru',
+            'Gender Male',
+            'Mobile 1234567890',
+            'Date of Birth 04 September,1985',
+            'Subjects Maths, Physics',
+            'Hobbies Sports, Reading, Music',
+            'Picture test_img.jpg',
+            'Address Nizhny Novgorod',
+            'State and City Haryana Panipat'
+        ))
+
+
+def test_form():
+    registration_page = RegistrationPage()
+    registration_page.open()
+
+    registration_page.fill_name()
+
+    registration_page.fill_email()
+
+    registration_page.choose_gender()
+
+    registration_page.fill_tel_number()
+
+    registration_page.fill_birthdate()
+
+    registration_page.fill_subjects()
+
+    registration_page.choose_hobies()
+
+    registration_page.uplod_picture()
+
+    registration_page.fill_current_address()
+
+    registration_page.choose_locatio()
+
+    registration_page.should_registered_user_whith()
