033b141279dff58f0a016f23fd109ed0cb6d4619
Silver <testingbugsrus@gmail.com>
2023-05-15 22:08:37 +0300

    - Refactoring method test_form

--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -6,42 +6,42 @@ from pages.registration_pages import RegistrationPage
 
 def test_form():
     registration_page = RegistrationPage()
-    user = Users('Aleksandr', 'Pushkin', 'Pushkin@proton.ru', 'Male', '1234567890', 'September', '1985', '4', 'Maths',
+    student = Users('Aleksandr', 'Pushkin', 'Pushkin@proton.ru', 'Male', '1234567890', 'September', '1985', '4', 'Maths',
                  'Physics', 'Sports, Reading, Music', 'test_img.jpg', 'Nizhny Novgorod', 'Haryana', 'Panipat')
 
     registration_page.open()
 
-    registration_page.fill_full_name(user.first_name, user.last_name)
+    registration_page.fill_full_name(student.first_name, student.last_name)
 
-    registration_page.fill_email(user.user_mail)
+    registration_page.fill_email(student.user_mail)
 
-    registration_page.choose_gender(user.gender)
+    registration_page.choose_gender(student.gender)
 
-    registration_page.fill_tel_number(user.tel_number)
+    registration_page.fill_tel_number(student.tel_number)
 
-    registration_page.fill_date_of_birth(user.month, user.year, user.day)
+    registration_page.fill_date_of_birth(student.month, student.year, student.day)
 
-    registration_page.fill_subjects(user.first_subject, user.second_subject)
+    registration_page.fill_subjects(student.first_subject, student.second_subject)
 
     registration_page.choose_hobies()
 
-    registration_page.uplod_picture(user.upload_picture_name)
+    registration_page.uplod_picture(student.upload_picture_name)
 
-    registration_page.fill_current_address(user.current_address)
+    registration_page.fill_current_address(student.current_address)
 
-    registration_page.choose_locatio(user.state, user.city)
+    registration_page.choose_locatio(student.state, student.city)
 
     registration_page.registered_user_data.should(
         have.exact_texts(
-            f'{user.first_name} {user.last_name}',
-            user.user_mail,
-            user.gender,
-            user.tel_number,
-            f'0{user.day} {user.month},{user.year}',
-            f'{user.first_subject}, {user.second_subject}',
-            user.hobbies,
-            user.upload_picture_name,
-            user.current_address,
-            f'{user.state} {user.city}'
+            f'{student.first_name} {student.last_name}',
+            student.user_mail,
+            student.gender,
+            student.tel_number,
+            f'0{student.day} {student.month},{student.year}',
+            f'{student.first_subject}, {student.second_subject}',
+            student.hobbies,
+            student.upload_picture_name,
+            student.current_address,
+            f'{student.state} {student.city}'
         )
     )
