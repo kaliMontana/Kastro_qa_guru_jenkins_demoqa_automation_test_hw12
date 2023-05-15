3490277d72a3a766f7888612691ccba31ca22cd9
Silver <testingbugsrus@gmail.com>
2023-05-15 22:01:27 +0300

    - Fix class Users

--- data/users.py
+++ data/users.py
@@ -6,12 +6,14 @@ class Users:
     first_name: str
     last_name: str
     user_mail: str
+    gender: str
     tel_number: str
     month: str
     year: str
     day: str
     first_subject: str
     second_subject: str
+    hobbies: str
     upload_picture_name: str
     current_address: str
     state: str
