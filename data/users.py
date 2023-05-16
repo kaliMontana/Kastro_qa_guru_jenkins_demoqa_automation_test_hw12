36a73055bcccbbc1595f3d585f37ba4b8d84266c
Silver <testingbugsrus@gmail.com>
2023-05-16 21:31:09 +0300

    - Create "student" instance of class Users

--- data/users.py
+++ data/users.py
@@ -18,3 +18,7 @@ class Users:
     current_address: str
     state: str
     city: str
+
+
+student = Users('Aleksandr', 'Pushkin', 'Pushkin@proton.ru', 'Male', '1234567890', 'September', '1985', '4', 'Maths',
+                'Physics', 'Sports, Reading, Music', 'test_img.jpg', 'Nizhny Novgorod', 'Haryana', 'Panipat')
