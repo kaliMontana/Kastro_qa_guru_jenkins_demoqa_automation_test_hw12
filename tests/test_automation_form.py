8a1e18e15cbfb942917d774d64954a6177b6a7f9
Silver <testingbugsrus@gmail.com>
2023-05-16 21:36:09 +0300

    - Refactoring test_form

--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -1,47 +1,13 @@
-from selene import have
-
-from data.users import Users
+from data import users
+from pages.profile_page import ProfilePage
 from pages.registration_pages import RegistrationPage
 
 
 def test_form():
+    student = users.student
     registration_page = RegistrationPage()
-    student = Users('Aleksandr', 'Pushkin', 'Pushkin@proton.ru', 'Male', '1234567890', 'September', '1985', '4', 'Maths',
-                 'Physics', 'Sports, Reading, Music', 'test_img.jpg', 'Nizhny Novgorod', 'Haryana', 'Panipat')
+    profile_page = ProfilePage()
 
     registration_page.open()
-
-    registration_page.fill_full_name(student.first_name, student.last_name)
-
-    registration_page.fill_email(student.user_mail)
-
-    registration_page.choose_gender(student.gender)
-
-    registration_page.fill_tel_number(student.tel_number)
-
-    registration_page.fill_date_of_birth(student.month, student.year, student.day)
-
-    registration_page.fill_subjects(student.first_subject, student.second_subject)
-
-    registration_page.choose_hobies()
-
-    registration_page.uplod_picture(student.upload_picture_name)
-
-    registration_page.fill_current_address(student.current_address)
-
-    registration_page.choose_locatio(student.state, student.city)
-
-    registration_page.registered_user_data.should(
-        have.exact_texts(
-            f'{student.first_name} {student.last_name}',
-            student.user_mail,
-            student.gender,
-            student.tel_number,
-            f'0{student.day} {student.month},{student.year}',
-            f'{student.first_subject}, {student.second_subject}',
-            student.hobbies,
-            student.upload_picture_name,
-            student.current_address,
-            f'{student.state} {student.city}'
-        )
-    )
+    registration_page.register(student)
+    profile_page.should_have_registered(student)
