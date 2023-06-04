f4a6cce001e846de06c2bc753e8b98aa697c6163
Silver <testingbugsrus@gmail.com>
2023-06-04 22:42:19 +0300

    Add allure attachment util

--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -1,9 +1,11 @@
 import allure
 from allure_commons.types import Severity
+from selene import browser
 
 from data import users
 from pages.profile_page import ProfilePage
 from pages.registration_pages import RegistrationPage
+from utils import attach
 
 
 @allure.tag('Web version')
@@ -20,3 +22,7 @@ def test_form():
     registration_page.open()
     registration_page.register(student)
     profile_page.should_have_registered(student)
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
