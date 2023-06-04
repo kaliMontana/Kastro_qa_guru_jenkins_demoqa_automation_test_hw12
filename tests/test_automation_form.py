076719d7906ee582e864b782ee135d87956acf55
Silver <testingbugsrus@gmail.com>
2023-06-04 11:06:47 +0300

    Add different allure decorators to test

--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -1,8 +1,17 @@
+import allure
+from allure_commons.types import Severity
+
 from data import users
 from pages.profile_page import ProfilePage
 from pages.registration_pages import RegistrationPage
 
 
+@allure.tag('Web version')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'Kastro')
+@allure.feature('Зарегитроировать пользователя')
+@allure.story('Пользователь вводит свои данные')
+@allure.link('http://demoqa.com/automation-practice-form', name='demoqa')
 def test_form():
     student = users.student
     registration_page = RegistrationPage()
