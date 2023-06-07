afc075862ca28e2821fa50d592b64290959807ed
Silver <testingbugsrus@gmail.com>
2023-06-07 19:48:45 +0300

    - Move from test_form selenoid capabilities and attaches to browser_setup in conftest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,34 @@
 import pytest
-from selene import browser
+from selene import Browser, Config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_setup():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com'
-    yield
+    options = Options()
+
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub/",
+        options=options
+    )
+    browser = Browser(Config(driver=driver, base_url='https://demoqa.com/', window_width=1920, window_height=1080))
+
+    yield browser
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
     browser.quit()
--- tests/test_automation_form.py
+++ tests/test_automation_form.py
@@ -1,13 +1,9 @@
 import allure
 from allure_commons.types import Severity
-from selene.support.shared import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 from data import users
 from pages.profile_page import ProfilePage
 from pages.registration_pages import RegistrationPage
-from utils import attach
 
 
 @allure.tag('Web version')
@@ -16,34 +12,13 @@ from utils import attach
 @allure.feature('Зарегитроировать пользователя')
 @allure.story('Пользователь вводит свои данные')
 @allure.link('http://demoqa.com/automation-practice-form', name='demoqa')
-def test_form():
-    options = Options()
-
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub/",
-        options=options
-    )
-    browser.config.driver = driver
+def test_form(browser_setup):
+    browser = browser_setup
 
     student = users.student
-    registration_page = RegistrationPage()
-    profile_page = ProfilePage()
+    registration_page = RegistrationPage(browser)
+    profile_page = ProfilePage(browser)
 
-    registration_page.open()
-    registration_page.register(student)
+    registration_page.open(browser)
+    registration_page.register(student, browser)
     profile_page.should_have_registered(student)
-
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
